f8f8e1da81e8617341b415be0b9ddf51a2e6b0cd
Manuel R Avila <manuelmuisca@gmail.com>
2024-04-15 21:10:37 -0400

    Updated base URL

--- excel_addin/src/taskpane/config.js
+++ excel_addin/src/taskpane/config.js
@@ -1,10 +1,20 @@
 // config.js
 (function() {
-    const devUrl = 'http://127.0.0.1:5000/';
-    const prodUrl = 'https://app.exmint.me/';
-    const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const devUrl = 'http://127.0.0.1:5000';
+    const prodUrl = 'https://app.exmint.me';
+    const stagingUrl = 'https://stg-addin.exmint.me';
+    const hostname = window.location.hostname;
+
+    let baseUrl;
+    if (hostname === 'localhost' || hostname === '127.0.0.1') {
+        baseUrl = devUrl;
+    } else if (hostname === 'stg-addin.exmint.me') {
+        baseUrl = stagingUrl;
+    } else {
+        baseUrl = prodUrl;
+    }
 
     window.appConfig = {
-        apiUrl: isDevelopment ? devUrl : prodUrl,
+        apiUrl: baseUrl,
     };
 })();
