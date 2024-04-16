6db04973e335f72d66016e018a0e9a564a5dd45c
Manuel R Avila <manuelmuisca@gmail.com>
2024-04-15 21:21:13 -0400

    Fixed base URL

--- excel_addin/src/taskpane/config.js
+++ excel_addin/src/taskpane/config.js
@@ -2,13 +2,13 @@
 (function() {
     const devUrl = 'http://127.0.0.1:5000';
     const prodUrl = 'https://app.exmint.me';
-    const stagingUrl = 'https://stg-app.exmint.me';
+    const stagingUrl = 'https://stg-app.exmint.me'; // This must be the app's URL 
     const hostname = window.location.hostname;
 
     let baseUrl;
     if (hostname === 'localhost' || hostname === '127.0.0.1') {
         baseUrl = devUrl;
-    } else if (hostname === 'stg-addin.exmint.me') {
+    } else if (hostname === 'stg-addin.exmint.me') { // This must be the add-in's URL 
         baseUrl = stagingUrl;
     } else {
         baseUrl = prodUrl;
