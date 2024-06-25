cf7e77a5a6546a9f855d2a8c8fafd6b61555fee8
Manuel R Avila <manuelmuisca@gmail.com>
2024-06-25 09:13:03 -0400

    Fixed logout

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 # views.py
-from flask import current_app, render_template, redirect, url_for, flash, request, jsonify, Blueprint, session
+from flask import current_app, render_template, redirect, url_for, flash, request, jsonify, Blueprint, session, make_response
 from flask_login import login_user, current_user, logout_user, login_required
 from extensions import mail
 from functools import wraps
@@ -187,15 +187,26 @@ def handle_unsuccessful_login(error_message):
 def logout():
     print(f"User logged in: {current_user.is_authenticated}")
     logout_user()
+    session.clear()
+
+    response = make_response()
+
+    # Clear any cookies related to authentication
+    response.set_cookie('session', '', expires=0)
+    response.set_cookie('remember_token', '', expires=0)
+
     print(f"User logged out: {not current_user.is_authenticated}")
     if request.method == 'POST':
         print("Request from Excel Add-in")
         return jsonify({'message': 'Logged out successfully'})
     else:
         print("Request from Flask UI")
-        response = redirect(url_for('views.index'))
-        response.set_cookie('token', '', expires=0)
-        return response
+        response = make_response(redirect(url_for('views.index')))
+
+    # Add headers to clear localStorage on the client side
+    response.headers['Clear-Site-Data'] = '"cache", "cookies", "storage"'
+        
+    return response
         
 @views.route('/reset_password', methods=['GET', 'POST'])
 # Use https://mailtrap.io/ for email for now .
