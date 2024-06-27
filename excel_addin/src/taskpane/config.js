c1ca6009c9059d26cc1ce53d3990e023108ffeb7
Manuel R Avila <manuelmuisca@gmail.com>
2024-06-27 10:16:53 -0400

    Last commit before tokens take over

--- excel_addin/src/taskpane/config.js
+++ excel_addin/src/taskpane/config.js
@@ -2,19 +2,26 @@
 (function() {
     const devUrl = 'http://127.0.0.1:5000';
     const prodUrl = 'https://app.exmint.me';
-    const stagingUrl = 'https://stg-app.exmint.me'; // This must be the app's URL 
+    const stagingUrl = 'https://stg-app.exmint.me'; 
+    const devSuffix = '-dev';
+    const prodSuffix = '';
+    const stagSuffix = '-stg'; 
     const hostname = window.location.hostname;
 
     let baseUrl;
     if (hostname === 'localhost' || hostname === '127.0.0.1') {
         baseUrl = devUrl;
+        suffix = devSuffix;
     } else if (hostname === 'stg-addin.exmint.me') { // This must be the add-in's URL 
         baseUrl = stagingUrl;
+        suffix = stagSuffix;
     } else {
         baseUrl = prodUrl;
+        suffix = prodSuffix;
     }
 
     window.appConfig = {
         apiUrl: baseUrl,
+        suffix: suffix,
     };
 })();
