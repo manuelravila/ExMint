20df537858664fe2efc6e604b7ec936ff0e12375
Manuel R Avila <manuelmuisca@gmail.com>
2025-01-02 15:58:15 -0500

    Fixed catalog creation

--- excel_addin/start_addin.sh
+++ excel_addin/start_addin.sh
@@ -5,14 +5,20 @@ FLASK_ENV=$(echo "$FLASK_ENV" | xargs)
 
 echo "Current FLASK_ENV: '$FLASK_ENV'"
 
+# Create catalog folder and remove old manifests
+mkdir -p ./app/dist/catalog
+rm -f ./app/dist/catalog/manifest*.xml
+
 # Use the correct directory for copying the manifest files
 if [ "$FLASK_ENV" = "stag" ]; then
     echo "Using staging manifest file"
     cp /app/dist/manifest-stag.xml /app/dist/manifest.xml
+    cp /app/dist/manifest-stag.xml /app/dist/catalog/manifest-stag.xml
 
 elif [ "$FLASK_ENV" = "main" ]; then
     echo "Using production manifest file"
     cp /app/dist/manifest-prod.xml /app/dist/manifest.xml
+    cp /app/dist/manifest-prod.xml /app/dist/catalog/manifest-prod.xml
 fi
 
 # Start the http-server to serve the application
